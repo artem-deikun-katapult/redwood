38b65da5b4eb745280e60a61931f825a85d21de0
Artem Deikun <artem.deikun@zorallabs.com>
2022-04-03 22:57:17 +0300

    Cell Articles

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,36 @@
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ articles }) => {
+  return (
+    <ul>
+      {articles.map(({ id, title, body, createdAt }) => {
+        return (
+          <article key={id}>
+            <header>
+              <h2>{title}</h2>
+            </header>
+            <p>{body}</p>
+            <div>Posted at: {createdAt}</div>
+          </article>
+        )
+      })}
+    </ul>
+  )
+}
