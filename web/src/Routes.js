fe84b5359e6629c99ca53aaba5d83a3fc3fceeb5
Artem Deikun <artem.deikun@zorallabs.com>
2022-04-04 00:18:38 +0300

    Contact Page

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,6 +14,7 @@ import BlogLayout from 'src/layouts/BlogLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/contact" page={ContactPage} name="contact" />
       <Set wrap={PostsLayout}>
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
@@ -23,6 +24,7 @@ const Routes = () => {
       <Set wrap={BlogLayout}>
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
       <Route notfound page={NotFoundPage} />
